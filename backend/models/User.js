a5e812b03843b235ff3def72e02d4c1124b51a3e
Sushil <shil7276@gmail.com>
2022-12-17 01:17:49 +0530

    password bcrypt and jwt security

--- backend/models/User.js
+++ backend/models/User.js
@@ -20,6 +20,6 @@ const UserSchema = new Schema({
         default: Date.now
     }
 });
+
 const User = mongoose.model("user", UserSchema);
-User.createIndexes();
 module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,30 +1,59 @@
 const express = require("express");
-
 const User = require("../models/User");
 
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-router.post("/", [
+const JWT_SECRET = "SushilisaGoodBoy";
+
+// create a User using: Post "/api/auth/createuser". No login required
+router.post("/createuser", [
     body('name', 'Enter valide name').isLength({ min: 3 }),
     body('email', 'Enter valide email').isEmail(),
     body('password', 'password gretter than 4 characters').isLength({ min: 4 })
-], (req, res) => {
+], async (req, res) => {
+
+    // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then(user => res.json(user))
-        .catch(err => {
-            console.log(err)
-            // send response error message for our understanding
-            res.json({ error: 'Please enter a unique value', message:err.message })  
 
-        })
-})
+    // Check whether the user with this email exists already
+    try {
+
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with this email already exists" })
+        }
+
+        const salt = await bcrypt.genSaltSync(10);
+        const secPass = await bcrypt.hashSync(req.body.password, salt);
+
+        // Create a new user
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: secPass,
+        });
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        const authtoken = jwt.sign(data, JWT_SECRET);
+
+        res.json({ authtoken: authtoken });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some Error occured");
+    }
+
+});
+
 
 module.exports = router
