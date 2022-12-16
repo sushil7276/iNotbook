1521417b73760a37a60c21b8d76f9fe4b9f0465e
Sushil <shil7276@gmail.com>
2022-12-16 12:50:19 +0530

    Validation Complete

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURL = "mongodb://localhost:27017/?directConnection=true";
+const mongoURL = "mongodb://localhost:27017/inotebook?directConnection=true";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURL, () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,12 +3,28 @@ const express = require("express");
 const User = require("../models/User");
 
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.post("/", (req, res) => {
-    console.log(req.body)
-    const user = User(req.body);
-    user.save()
-    res.send(req.body);
+router.post("/", [
+    body('name', 'Enter valide name').isLength({ min: 3 }),
+    body('email', 'Enter valide email').isEmail(),
+    body('password', 'password gretter than 4 characters').isLength({ min: 4 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+        .catch(err => {
+            console.log(err)
+            // send response error message for our understanding
+            res.json({ error: 'Please enter a unique value', message:err.message })  
+
+        })
 })
 
 module.exports = router
